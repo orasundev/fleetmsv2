91eb1412b0fa3d82a43f8f7b8a9c17383a3e7744
orasundev <orasundev@gmail.com>
2022-05-30 09:54:13 +0500

    First Commit

---
+++ src/main/java/com/ahcode/fleetms/ApplicationController.java
@@ -0,0 +1,18 @@
+package com.ahcode.fleetms;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+
+    @GetMapping("/index")
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/widgets")
+    public String widget(){
+        return "widgets.html";
+    }
+}
